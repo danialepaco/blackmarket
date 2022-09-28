0c38e82d899005031e16f5eba3b89e080c90380c
Daniel <danialepaco@hotmail.com>
2022-09-27 22:12:55 -0400

    init repo

---
+++ blackmarket/blackmarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  blackmarketApp.swift
+//  blackmarket
+//
+//  Created by Daniel Parra on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct blackmarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
